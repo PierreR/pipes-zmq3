34ffd16c42a5f1b8a1ad59fc75813ebb6768c82e
PierreR <pierrer@pi3r.be>
2013-09-02 00:11:45 +0200

    Add a report line every 10 records with the sum of temperature

--- sub.hs
+++ sub.hs
@@ -2,16 +2,18 @@ module Main
 where
 
 import Pipes
+import Pipes.Parse 
 import qualified Pipes.Prelude as P
 import qualified Pipes.ZMQ3 as PZ
 import qualified System.ZMQ3 as Z
 
 import Control.Concurrent(threadDelay)
 import Control.Concurrent.Async
-import Control.Monad (forever)
-import Data.ByteString.Char8 (pack, unpack)
-import System.Random (randomRIO)
+import Control.Monad (forever, unless)
 
+import Data.ByteString.Char8 (pack, unpack)
+import Text.Printf
+import System.Random (randomRIO)
 
 main :: IO ()
 main = do
@@ -25,17 +27,34 @@ main = do
             async $ pubServer pubSocket
             Z.connect subSocket "inproc://pubserver"
             Z.subscribe subSocket (pack "10001")
-            runEffect $ PZ.fromSub subSocket >-> P.map (display.words.unpack) >->  P.stdout
-    where
-        
+            evalStateT loop (processedData subSocket)
+    where 
+
+        loop :: StateT (Producer (Int, Int, Int) IO  r) IO ()
+        loop = do
+            temp <- P.sum (input >-> P.take 10 >-> P.map (\(_, t, _) -> t))
+            lift $ printf "-- Report: sum temp is %d --" temp
+            eof <- isEndOfInput
+            unless eof loop
+
         pubServer s = forever $ do
-            threadDelay (29) -- be gentle with the CPU
-            zipcode <- randomRIO (10000::Int, 19900) -- be gentle with the CPU
+            threadDelay (28) -- be gentle with the CPU
+            zipcode <- randomRIO (10000::Int, 11000)
             temperature <- randomRIO (-10::Int, 35)
             humidity <- randomRIO (10::Int, 60)
             let update = pack $ unwords [show zipcode, show temperature, show humidity]
             Z.send s [] update
         
+        processedData :: Z.Socket Z.Sub -> Producer (Int, Int, Int) IO ()
+        processedData subSocket = for (PZ.fromSub subSocket) $ \bs -> do
+            let ws = words (unpack bs)
+            liftIO $ putStrLn (display ws)
+            let [zipcode, temperature, humidity] = map read ws
+            yield (zipcode, temperature, humidity)
+
         display:: [String] -> String
         display [_, temp, hum] =
-            unwords ["NY City", "Temperature:" , temp, "Humidity:" , hum] 
+            unwords ["NY City", "Temperature:" , temp, "Humidity:" , hum]
+
+
+
